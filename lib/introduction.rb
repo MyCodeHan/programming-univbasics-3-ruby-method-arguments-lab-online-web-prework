d8cfd3ebdb35b99b2ad3a104133abaed8f46d1a0
Hannah Harrington <hannahharrington@outlook.com>
2019-08-04 19:08:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,13 @@
 def myName(name)
   "Hi, my name is #{name}"
 end
+
 #introduction_with_language
-def myName name, myLanguage
-  "Hi, my name is #{name} and I am learning to program in#{language}."
+def myName name, language
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+#introduction_with_language_optional
+def myName name, language=Ruby
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
